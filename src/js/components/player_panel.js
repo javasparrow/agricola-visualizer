2aecfab4c41d77c8b738f7640752f4b8aa1c760e
ODA <twbtarai.blackpepper@gmail.com>
2019-07-07 04:54:47 +0900

    add future

--- src/js/components/player_panel.js
+++ src/js/components/player_panel.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import ResourcePanel from "./resource_panel";
 import PlayerBoard from "./player_board";
 import { player_colors } from "../consts/colors";
+import { strict } from "assert";
 
 export default class PlayerPanel extends React.Component {
 
@@ -37,6 +38,23 @@ export default class PlayerPanel extends React.Component {
       <div style={{marginLeft: 16}}>{this.props.json_data.played_improvements.map(improvement => improvement.improvement_id).join(",")}</div>
       <div>Occupations</div>
       <div style={{marginLeft: 16}}>{this.props.json_data.played_occupations.map(occupation => occupation.occupation_id).join(",")}</div>
+      <table>
+        <tr>
+          {Array.from(Array(14).keys()).map((round) => <th style={{"border": "1px solid #DDDDDD", "padding": 4}}>{round + 1}</th>)}
+        </tr>
+        <tr>
+          {
+            Array.from(Array(14).keys()).map((round) => {
+              if (this.props.json_data.round_resources[`${round + 1}`]){
+                return <th style={{"border": "1px solid #DDDDDD", "padding": 4}}>{
+                  JSON.stringify(this.props.json_data.round_resources[`${round + 1}`])
+                }</th>
+              }
+              return <th style={{"border": "1px solid #DDDDDD", "padding": 4}}></th>
+            })
+          }
+        </tr>
+      </table>
       <ResourcePanel resources={this.props.json_data.resources} />
       <PlayerBoard board={this.props.json_data.board} fences={this.props.json_data.fences} />
     </div>
