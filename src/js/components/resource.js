a06b9856f6de339b1857a005ce05ab3dafba582a
ODA <twbtarai.blackpepper@gmail.com>
2019-07-27 03:13:20 +0900

    add color to

--- src/js/components/resource.js
+++ src/js/components/resource.js
@@ -15,6 +15,19 @@ const resource_type_map = {
   veg: <GiCarrot />
 }
 
+const resource_color = {
+  wood: "#8B4513",
+  boar: "#000000",
+  cattle: "#8B4513",
+  clay: "#CD5C5C",
+  food: "#CCCC00",
+  grain: "#CCCC00",
+  reed: "#666666",
+  sheep: "#666666",
+  stone: "#000000",
+  veg: "#FF8C00"
+}
+
 export default class Resource extends React.Component {
 
   constructor(props) {
@@ -23,7 +36,7 @@ export default class Resource extends React.Component {
 
   render() {
     return (
-        <div style={{display: "inline-block", margin: 10}}> 
+        <div style={{display: "inline-block", margin: 10, color: resource_color[this.props.resource_type]}}> 
           {resource_type_map[this.props.resource_type]}:{this.props.resource_amount} 
         </div>
     );
